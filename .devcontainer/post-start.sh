3312307620f4094ca98448317cb1e150159cb1a6
vmrangelf <31077729+vmrangelf@users.noreply.github.com>
2024-04-17 08:18:17 -0600

    Initial commit

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "post-start start" >> ~/status.log
+
+# this runs in background each time the container starts
+
+kind export kubeconfig --name dev
+
+bash .hack/update-repo-for-workshop.sh
+
+source ~/.bashrc
+
+echo "post-start complete" >> ~/status.log
